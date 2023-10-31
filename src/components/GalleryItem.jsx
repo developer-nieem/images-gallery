5602661c290438ffc1bba5f1786d01c041407f80
developer-nieem <naimhossenwork@gmail.com>
2023-10-31 18:30:02 +0600

    setup project for images gallery

--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -1,14 +1,6 @@
 const GalleryItem = ({ src, selected, onToggle }) => {
   return (
-    <div>
-      <img src={src} alt="Gallery Item" />
-      <input
-        type="checkbox"
-        className="select-checkbox"
-        checked={selected}
-        onChange={onToggle}
-      />
-    </div>
+    
   );
 };
 
